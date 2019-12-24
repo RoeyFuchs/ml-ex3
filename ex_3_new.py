a177f6edda29b34c39f62045b5db3b0752473476
fuchsroey <fuchsroey@gmail.com>
2019-12-24 20:49:10 +0200

    60% add write to file

--- ex_3_new.py
+++ ex_3_new.py
@@ -5,7 +5,7 @@ from scipy.special import softmax
 # np.seterr(all='raise')
 
 sigmoid = lambda x: 1 / (1 + np.exp(-x))
-sigmoid_prime = lambda x: sigmoid(x)*(1-sigmoid(x))
+sigmoid_prime = lambda x: sigmoid(x) * (1 - sigmoid(x))
 relu_prime = lambda x: (x > 0) * 1
 
 
@@ -23,7 +23,7 @@ def initilize(start_shape):
     num_of_nurons_1 = 250
     num_of_nurons_2 = 100
     num_of_labels = 10
-    uniform_range = 4
+    uniform_range = 3
     w1 = np.random.uniform(-uniform_range, uniform_range, ([start_shape, num_of_nurons_1]))
     w2 = np.random.uniform(-uniform_range, uniform_range, ([num_of_nurons_1, num_of_nurons_2]))
     w3 = np.random.uniform(-uniform_range, uniform_range, ([num_of_nurons_2, num_of_labels]))
@@ -36,7 +36,8 @@ def initilize(start_shape):
     b2 = np.random.uniform(-1, 1, ([1, num_of_nurons_2]))
     b3 = np.random.uniform(-1, 1, ([1, num_of_labels]))
 
-    return {'w1': w1.copy(), 'w2': w2.copy(), 'w3': w3.copy(), 'b1': b1.copy(), 'b2':b2.copy(), 'b3':b3.copy()}
+    return {'w1': w1.copy(), 'w2': w2.copy(), 'w3': w3.copy(), 'b1': b1.copy(), 'b2': b2.copy(), 'b3': b3.copy()}
+
 
 def train(x_data, y_data, ephocs=40):
     x_data, y_data, x_valid, y_valid = split(x_data, y_data)
@@ -63,7 +64,8 @@ def train(x_data, y_data, ephocs=40):
                 bprop_vals = bprop(fprop_vals)
 
                 #  update
-                learning_rate = 0.002
+                # learning_rate = 0.001
+                learning_rate = 1 / 255
                 w1 -= learning_rate * bprop_vals['w1']
                 w2 -= learning_rate * bprop_vals['w2']
                 w3 -= learning_rate * bprop_vals['w3']
@@ -79,15 +81,16 @@ def train(x_data, y_data, ephocs=40):
             if np.argmax(fprop_vals['h3']) == y:
                 correct += 1
         if (correct / y_valid.shape[0]) > best:
-            best = correct/y_valid.shape[0]
+            best = correct / y_valid.shape[0]
             best_w1 = w1.copy()
             best_w2 = w2.copy()
             best_w3 = w3.copy()
             best_b1 = b1.copy()
             best_b2 = b2.copy()
             best_b3 = b3.copy()
-        print("best: "+str(best))
+        print("best: " + str(best))
 
+    return {'w1': best_w1, 'w2': best_w2, 'w3': best_w3, 'b1': best_b1, 'b2': best_b2, 'b3': best_b3}
 
 
 def fprop(x, y, params):
@@ -128,10 +131,31 @@ def bprop(fprop_vals):
     return {'b1': db1.T, 'w1': dW1.T, 'b2': db2.T, 'w2': dW2.T, 'w3': dW3.T, 'b3': db3.T}
 
 
+def predicte(x_test, best_values):
+    w1, b1, w2, b2, w3, b3 = [best_values[key] for key in ('w1', 'b1', 'w2', 'b2', 'w3', 'b3')]
+
+    for x in x_test:
+        z1 = np.dot(w1.T, np.array([x]).T) + b1.T
+        h1 = relu(z1.copy())
+        z2 = np.dot(w2.T, h1) + b2.T
+        h2 = relu(z2.copy())
+        z3 = np.dot(w3.T, h2) + b3.T
+        h3 = softmax(z3.copy())
+        pred = np.argmax(h3)
+        with open('test_y', 'a') as file:
+            file.write(str(pred)+'\n')
+
+
+
+
+
+
 def main():
     x = normalize(load_file(sys.argv[1]))
     y = load_file(sys.argv[2])
-    train(x, y)
+    best_values = train(x, y)
+    x_test = normalize(load_file(sys.argv[3]))
+    predicte(x_test, best_values)
 
 
 if __name__ == "__main__":
